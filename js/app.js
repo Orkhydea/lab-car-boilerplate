1fa02e74f48aa77448142b9ddfb4ce9d6a8f3d35
Orquídea Campos <37587788+Orkhydea@users.noreply.github.com>
2018-06-19 10:55:50 -0500

    first commit

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
 // La funcionalidad de tu proyecto
+//document.addEventListener('DOMContentLoaded', function() {
+   // var elems = document.querySelectorAll('.sidenav');
+    //var instances = M.Sidenav.init(elems, options);
+  //});
+
+  // Or with jQuery
+
+  //$(document).ready(function(){
+   // $('.sidenav').sidenav();
+  //});
+ 
+       
